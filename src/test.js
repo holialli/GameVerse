565320a66a023aad6a35f2ab008bce02aeab070d
Ali Ahmad <142441350+holialli@users.noreply.github.com>
2026-01-29 21:35:09 +0500

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,1 +1,3 @@
-const AWS_SECRET_KEY = "AKIAIMNO7CQ9HAEXAMPLE";
+// This looks like a real key but is random
+const AWS_KEY = "AKIAV3J5B7R9L2M4P6X1"; 
+const AWS_SECRET = "vX9bH2jK7mN4pL6qR8sT1uW3xY5zA7bC9dE1fG3h";
